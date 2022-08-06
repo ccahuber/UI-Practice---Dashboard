25351a87ee614fd44920c92fa18962cd1a741932
Christopher C.A Huber <c.c.a.huber@gmail.com>
2022-08-05 19:38:35 -0700

    Final Project
    Project Complete - JS added for some functionality

---
+++ orders.js
@@ -0,0 +1,43 @@
+const Orders = [{
+        productName: 'Foldable Mini Drone',
+        productNumber: '8333',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lawn Chair',
+        productNumber: '33323',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Camping Pans',
+        productNumber: '34333',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Battery Charger',
+        productNumber: '34343',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Running Mat',
+        productNumber: '43443',
+        paymentStatus: 'Due',
+        shipping: 'success'
+    },
+    {
+        productName: 'Battery Holder',
+        productNumber: '34334',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Towel',
+        productNumber: '84843',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    }
+]
